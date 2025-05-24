aa9c500582713e9ae6d6a0e1d1cea9ca463e6016
bailib <littlebaiw@qq.com>
2025-05-24 11:03:04 +0800

    Update SettingLoad.lua

--- SettingLoad.lua
+++ SettingLoad.lua
@@ -1,6 +1,6 @@
 local Games = {
-  [116495829188952] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Dead-RailV5.lua", -- Dead Rails
-  [70876832253163] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Dead-RailV5.lua", -- Dead Rails
+  [116495829188952] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Dead-RailV7.lua", -- Dead Rails
+  [70876832253163] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Dead-RailV7.lua", -- Dead Rails
   [18687417158] = "https://raw.githubusercontent.com/XiaoyeQWQ/VEP/refs/heads/main/Forsaken.lua", -- Forsaken
   [2210085102] = "https://raw.githubusercontent.com/XiaoyeQWQ/game/refs/heads/main/Rise-of-the-Legend-Hub-Naval-Warfare.lucky",--NavalWarfare
   [126884695634066] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/GrowAGarden.txt",--Grow a garden
