0c7e74a82acecf071222b51c188f23758fa47be7
bailib <littlebaiw@qq.com>
2025-06-22 09:15:45 +0800

    Update list.lua
    Signed-off-by: bailib <littlebaiw@qq.com>

--- loader/list.lua
+++ loader/list.lua
@@ -1,1 +1,5 @@
-
+local game = {
+  ["Brainrot Evolution.luau"] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Game/Brainrot%20Evolution.luau",
+  ["Animal Hide and Seek.luau"] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Game/Animal%20Hide%20and%20Seek.luau"
+}
+return game
