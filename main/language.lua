5c77b6c92e8a3f41df3d4dfebf3915a9625ae146
bailib <littlebaiw@qq.com>
2025-06-22 11:05:27 +0800

    Create language.lua
    Signed-off-by: bailib <littlebaiw@qq.com>

---
+++ main/language.lua
@@ -0,0 +1,203 @@
+local language = {
+  ["zh-cn"] = {
+    Author = "制作者:默小白",
+    OpenText = "打开UI",
+    NotificationText = "开源项目",
+    NotificationContent = "作者：默小白",
+    ["Animal Hide and Seek"] = {
+      Title = "动物捉迷藏",
+      AimbotHider = "自瞄躲藏者",
+      ESPHider = "透视躲藏者",
+      AutoCoin = "自动收集硬币",
+      AlwaysSeeker = "永远成为搜查者",
+      AutoWalk = "自动走向躲藏者"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "布洛雷特进化",
+      AttackAll = "攻击全部",
+      KillAura = "杀戮光环",
+      TpAttack = "传送攻击"
+    }
+  },
+  ["en-us"] = {
+    Author = "Creator: Moxiaobai",
+    OpenText = "Open UI",
+    NotificationText = "Open source",
+    NotificationContent = "By Moxiaobai",
+    ["Animal Hide and Seek"] = {
+      Title = "Animal Hide and Seek",
+      AimbotHider = "Aimbot Hider",
+      ESPHider = "ESP Hider",
+      AutoCoin = "Auto Collect Coins",
+      AlwaysSeeker = "Always Be Seeker",
+      AutoWalk = "Auto Walk to Hider"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "Brainrot Evolution",
+      AttackAll = "Attack All",
+      KillAura = "Kill Aura",
+      TpAttack = "Teleport Attack"
+    }
+  },
+  ["en-gb"] = {
+    Author = "Creator: Moxiaobai",
+    OpenText = "Open UI",
+    NotificationText = "Open source",
+    NotificationContent = "By Moxiaobai",
+    ["Animal Hide and Seek"] = {
+      Title = "Animal Hide and Seek",
+      AimbotHider = "Aimbot Hider",
+      ESPHider = "ESP Hider",
+      AutoCoin = "Auto Collect Coins",
+      AlwaysSeeker = "Always Be Seeker",
+      AutoWalk = "Auto Walk to Hider"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "Brainrot Evolution",
+      AttackAll = "Attack All",
+      KillAura = "Kill Aura",
+      TpAttack = "Teleport Attack"
+    }
+  },
+  ["ru-ru"] = {
+    Author = "Создатель: Moxiaobai",
+    OpenText = "Открыть UI",
+    NotificationText = "Открытый исходный код",
+    NotificationContent = "Автор: Moxiaobai",
+    ["Animal Hide and Seek"] = {
+      Title = "Животные в прятках",
+      AimbotHider = "Aimbot прячущийся",
+      ESPHider = "ESP прячущийся",
+      AutoCoin = "Авто сбор монет",
+      AlwaysSeeker = "Всегда ищущий",
+      AutoWalk = "Авто ходьба к прячущемуся"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "Эволюция Brainrot",
+      AttackAll = "Атаковать всех",
+      KillAura = "Аура убийства",
+      TpAttack = "Телепорт атака"
+    }
+  },
+  ["es-es"] = {
+    Author = "Creador: Moxiaobai",
+    OpenText = "Abrir UI",
+    NotificationText = "Código abierto",
+    NotificationContent = "Por Moxiaobai",
+    ["Animal Hide and Seek"] = {
+      Title = "Escondite Animal",
+      AimbotHider = "Aimbot escondido",
+      ESPHider = "ESP escondido",
+      AutoCoin = "Recoger monedas automático",
+      AlwaysSeeker = "Siempre buscador",
+      AutoWalk = "Caminar automático al escondido"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "Evolución Brainrot",
+      AttackAll = "Atacar a todos",
+      KillAura = "Aura mortal",
+      TpAttack = "Ataque de teletransporte"
+    }
+  },
+  ["es-mx"] = {
+    Author = "Creador: Moxiaobai",
+    OpenText = "Abrir UI",
+    NotificationText = "Código abierto",
+    NotificationContent = "Por Moxiaobai",
+    ["Animal Hide and Seek"] = {
+      Title = "Escondidas Animales",
+      AimbotHider = "Aimbot escondido",
+      ESPHider = "ESP escondido",
+      AutoCoin = "Recoger monedas automático",
+      AlwaysSeeker = "Siempre buscador",
+      AutoWalk = "Caminar automático al escondido"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "Evolución Brainrot",
+      AttackAll = "Atacar a todos",
+      KillAura = "Aura mortal",
+      TpAttack = "Ataque de teletransporte"
+    }
+  },
+  ["zh-tw"] = {
+    Author = "製作者:默小白",
+    OpenText = "打開UI",
+    NotificationText = "開源項目",
+    NotificationContent = "作者：默小白",
+    ["Animal Hide and Seek"] = {
+      Title = "動物捉迷藏",
+      AimbotHider = "自瞄躲藏者",
+      ESPHider = "透視躲藏者",
+      AutoCoin = "自動收集硬幣",
+      AlwaysSeeker = "永遠成為搜查者",
+      AutoWalk = "自動走向躲藏者"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "布洛雷特進化",
+      AttackAll = "攻擊全部",
+      KillAura = "殺戮光環",
+      TpAttack = "傳送攻擊"
+    }
+  },
+  ["zh-hk"] = {
+    Author = "製作者:默小白",
+    OpenText = "打開UI",
+    NotificationText = "開源項目",
+    NotificationContent = "作者：默小白",
+    ["Animal Hide and Seek"] = {
+      Title = "動物捉迷藏",
+      AimbotHider = "自瞄躲藏者",
+      ESPHider = "透視躲藏者",
+      AutoCoin = "自動收集硬幣",
+      AlwaysSeeker = "永遠成為搜查者",
+      AutoWalk = "自動走向躲藏者"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "布洛雷特進化",
+      AttackAll = "攻擊全部",
+      KillAura = "殺戮光環",
+      TpAttack = "傳送攻擊"
+    }
+  },
+  ["ja-jp"] = {
+    Author = "作成者: 默小白",
+    OpenText = "UIを開く",
+    NotificationText = "オープンソース",
+    NotificationContent = "作者: 默小白",
+    ["Animal Hide and Seek"] = {
+      Title = "アニマルハイドアンドシーク",
+      AimbotHider = "エイムボット隠れ",
+      ESPHider = "ESP隠れ",
+      AutoCoin = "自動コイン収集",
+      AlwaysSeeker = "常にシーカー",
+      AutoWalk = "自動で隠れに向かう"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "ブレインロット進化",
+      AttackAll = "全員攻撃",
+      KillAura = "キルオーラ",
+      TpAttack = "テレポート攻撃"
+    }
+  },
+  ["de-de"] = {
+    Author = "Ersteller: Moxiaobai",
+    OpenText = "UI öffnen",
+    NotificationText = "Open Source",
+    NotificationContent = "Von Moxiaobai",
+    ["Animal Hide and Seek"] = {
+      Title = "Tier Verstecken",
+      AimbotHider = "Aimbot Verstecker",
+      ESPHider = "ESP Verstecker",
+      AutoCoin = "Automatische Münzen",
+      AlwaysSeeker = "Immer Sucher",
+      AutoWalk = "Automatisch zum Verstecker"
+    },
+    ["Brainrot Evolution"] = {
+      Title = "Brainrot Evolution",
+      AttackAll = "Alle angreifen",
+      KillAura = "Todesaura",
+      TpAttack = "Teleport Angriff"
+    }
+  }
+}
+return language
